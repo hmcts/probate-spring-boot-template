88fe84ac1917aa0747c3ae2fd4c4c82382efd439
Dhruv Patel <dhruv.patel20@gmail.com>
2018-10-30 16:19:16 +0000

    PRO-4035: Added gradle wrapper.

---
+++ src/main/java/uk/gov/hmcts/probate/SpringBootTemplateApplication.java
@@ -0,0 +1,8 @@
+package uk.gov.hmcts.probate;
+
+public class SpringBootTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTemplateApplication.class, args);
+    }
+}
