d275de34b9609c8d97e60f414c02ac7f80ca74a4
Dhruv Patel <dhruv.patel20@gmail.com>
2018-10-31 15:11:47 +0000

    PRO-4035: Cleaned up gradle dependencies and ensued app can start up.

--- src/main/java/uk/gov/hmcts/probate/SpringBootTemplateApplication.java
+++ src/main/java/uk/gov/hmcts/probate/SpringBootTemplateApplication.java
@@ -1,10 +1,15 @@
 package uk.gov.hmcts.probate;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
+@SpringBootApplication
+@Configuration
 public class SpringBootTemplateApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootTemplateApplication.class, args);
     }
 }
+                                                                              
