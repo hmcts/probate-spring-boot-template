9e324ae048bcc5280f2883544077ace730de23f9
Dhruv Patel <dhruv.patel20@gmail.com>
2018-11-02 13:00:35 +0000

    PRO-4035: Removed unnecessary configuration annotation.

--- src/main/java/uk/gov/hmcts/probate/SpringBootTemplateApplication.java
+++ src/main/java/uk/gov/hmcts/probate/SpringBootTemplateApplication.java
@@ -2,10 +2,8 @@ package uk.gov.hmcts.probate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-@Configuration
 public class SpringBootTemplateApplication {
 
     public static void main(String[] args) {
